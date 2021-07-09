6d3e43680c8ba44e232c2c8a9dcee31a7aefdde4
Frederik Ring <frederik.ring@gmail.com>
2021-07-09 09:21:42 +0200

    add basic integration test

--- test/default/test.sh
+++ test/default/test.sh
@@ -2,4 +2,17 @@
 
 set -e
 
-echo "all is well"
+cd $(dirname $0)
+
+docker-compose up -d
+sleep 5
+
+docker-compose exec backup backup
+
+docker run --rm -it \
+  -v default_backup_data:/data alpine \
+  ash -c 'tar -xf /data/backup/test.tar.gz && test -f /backup/app_data/offen.db'
+
+docker-compose down --volumes
+
+echo "Test passed"
