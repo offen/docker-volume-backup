d29d0d73998753c4ddf48948986f2697ea5c39fb
Frederik Ring <frederik.ring@gmail.com>
2021-07-11 10:26:35 +0200

    check for correct file in tests

--- test/swarm/run.sh
+++ test/swarm/run.sh
@@ -19,7 +19,7 @@ docker exec $(docker ps -q -f name=backup) backup
 
 docker run --rm -it \
   -v test_stack_backup_data:/data alpine \
-  ash -c 'tar -xf /data/backup/test.tar.gz && test -f /backup/pg_data/pg_stat/global.stat'
+  ash -c 'tar -xf /data/backup/test.tar.gz && test -f /backup/pg_data/pg_stat/PG_VERSION'
 
 echo "[TEST:PASS] Found relevant files in untared backup."
 
