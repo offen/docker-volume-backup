8b7d27740ad5a4daa09e8e5e745266066ceb7476
Frederik Ring <frederik.ring@gmail.com>
2021-08-21 21:26:27 +0200

    implement copy to remote storage

--- go.mod
+++ go.mod
@@ -10,17 +10,37 @@ require (
 	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/klauspost/cpuid v1.3.1 // indirect
+	github.com/minio/md5-simd v1.1.0 // indirect
+	github.com/minio/minio-go v6.0.14+incompatible
+	github.com/minio/minio-go/v7 v7.0.12 // indirect
+	github.com/minio/sha256-simd v0.1.1 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/xid v1.2.1 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887 // indirect
+	golang.org/x/text v0.3.4 // indirect
 	google.golang.org/genproto v0.0.0-20201110150050-8816d57aaa9a // indirect
 	google.golang.org/grpc v1.33.2 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/ini.v1 v1.57.0 // indirect
+)
+
+require (
+	github.com/go-ini/ini v1.25.4 // indirect
+	github.com/walle/targz v0.0.0-20140417120357-57fe4206da5a // indirect
 )
--- src/main.go
+++ src/main.go
@@ -4,66 +4,42 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"os"
-	"time"
+	"path"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/swarm"
 	"github.com/docker/docker/client"
 	"github.com/joho/godotenv"
+	minio "github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/walle/targz"
 )
 
 func main() {
 	s := &script{}
-	defer func() {
-		for _, thunk := range s.cleanupTasks {
-			thunk()
-		}
-	}()
 
 	s.lock()
 	defer s.unlock()
 
-	if err := s.init(); err != nil {
-		panic(err)
-	}
-
-	if err := s.stopContainers(); err != nil {
-		panic(err)
-	}
-
-	if err := s.takeBackup(); err != nil {
-		panic(err)
-	}
-
-	if err := s.restartContainers(); err != nil {
-		panic(err)
-	}
-
-	if err := s.encryptBackup(); err != nil {
-		panic(err)
-	}
-
-	if err := s.copyBackup(); err != nil {
-		panic(err)
-	}
-
-	if err := s.cleanBackup(); err != nil {
-		panic(err)
-	}
-
-	if err := s.pruneBackups(); err != nil {
-		panic(err)
-	}
+	must(s.init)()
+	must(s.stopContainers)()
+	must(s.takeBackup)()
+	must(s.restartContainers)()
+	must(s.encryptBackup)()
+	must(s.copyBackup)()
+	must(s.cleanBackup)()
+	must(s.pruneBackups)()
 }
 
 type script struct {
 	ctx               context.Context
 	cli               *client.Client
+	mc                *minio.Client
 	stoppedContainers []types.Container
 	file              string
-	cleanupTasks      []func()
 }
 
 func (s *script) lock()   {}
@@ -71,15 +47,6 @@ func (s *script) unlock() {}
 
 func (s *script) init() error {
 	s.ctx = context.Background()
-	if timeout, ok := os.LookupEnv("BACKUP_TIMEOUT_DURATION"); ok {
-		d, err := time.ParseDuration(timeout)
-		if err != nil {
-			return fmt.Errorf("init: error parsing given timeout duration: %w", err)
-		}
-		withTimeout, cancelFunc := context.WithTimeout(context.Background(), d)
-		s.ctx = withTimeout
-		s.cleanupTasks = append(s.cleanupTasks, cancelFunc)
-	}
 
 	if err := godotenv.Load("/etc/backup.env"); err != nil {
 		return fmt.Errorf("init: failed to load env file: %w", err)
@@ -96,6 +63,21 @@ func (s *script) init() error {
 		}
 		s.cli = cli
 	}
+
+	if bucket := os.Getenv("AWS_S3_BUCKET_NAME"); bucket != "" {
+		mc, err := minio.New(os.Getenv("AWS_ENDPOINT"), &minio.Options{
+			Creds: credentials.NewStaticV4(
+				os.Getenv("AWS_ACCESS_KEY_ID"),
+				os.Getenv("AWS_SECRET_ACCESS_KEY"),
+				"",
+			),
+			Secure: os.Getenv("AWS_ENDPOINT_PROTO") == "https",
+		})
+		if err != nil {
+			return fmt.Errorf("init: error setting up minio client: %w", err)
+		}
+		s.mc = mc
+	}
 	return nil
 }
 
@@ -111,13 +93,12 @@ func (s *script) stopContainers() error {
 		}),
 	})
 
-	s.stoppedContainers = stoppedContainers
 	if err != nil {
 		return fmt.Errorf("stopContainers: error querying for containers to stop: %w", err)
 	}
-	fmt.Printf("Stopping %d containers\n", len(s.stoppedContainers))
+	fmt.Printf("Stopping %d containers\n", len(stoppedContainers))
 
-	if len(s.stoppedContainers) != 0 {
+	if len(stoppedContainers) != 0 {
 		fmt.Println("Stopping containers")
 		for _, container := range s.stoppedContainers {
 			if err := s.cli.ContainerStop(s.ctx, container.ID, nil); err != nil {
@@ -125,11 +106,21 @@ func (s *script) stopContainers() error {
 			}
 		}
 	}
+
+	s.stoppedContainers = stoppedContainers
 	return nil
 }
 
 func (s *script) takeBackup() error {
-	return errors.New("takeBackup: not implemented yet")
+	file := os.Getenv("BACKUP_FILENAME")
+	if file == "" {
+		return errors.New("takeBackup: BACKUP_FILENAME not given")
+	}
+	s.file = file
+	if err := targz.Compress(os.Getenv("BACKUP_SOURCES"), s.file); err != nil {
+		return fmt.Errorf("takeBackup: error compressing backup folder: %w", err)
+	}
+	return nil
 }
 
 func (s *script) restartContainers() error {
@@ -169,24 +160,43 @@ func (s *script) restartContainers() error {
 }
 
 func (s *script) encryptBackup() error {
-	_, ok := os.LookupEnv("GPG_PASSPHRASE")
-	if !ok {
+	key := os.Getenv("GPG_PASSPHRASE")
+	if key == "" {
 		return nil
 	}
 	return errors.New("encryptBackup: not implemented yet")
 }
 
 func (s *script) copyBackup() error {
-	return errors.New("copyBackup: not implemented yet")
+	_, name := path.Split(s.file)
+	if bucket := os.Getenv("AWS_S3_BUCKET_NAME"); bucket != "" {
+		_, err := s.mc.FPutObject(s.ctx, bucket, name, s.file, minio.PutObjectOptions{
+			ContentType: "application/tar+gzip",
+		})
+		if err != nil {
+			return fmt.Errorf("copyBackup: error uploading backup to remote storage: %w", err)
+		}
+	}
+	if archive := os.Getenv("BACKUP_ARCHIVE"); archive != "" {
+		if _, err := os.Stat(archive); !os.IsNotExist(err) {
+			if err := copy(s.file, path.Join(archive, name)); err != nil {
+				return fmt.Errorf("copyBackup: error copying file to local archive: %w", err)
+			}
+		}
+	}
+	return nil
 }
 
 func (s *script) cleanBackup() error {
-	return errors.New("cleanBackup: not implemented yet")
+	if err := os.Remove(s.file); err != nil {
+		return fmt.Errorf("cleanBackup: error removing file: %w", err)
+	}
+	return nil
 }
 
 func (s *script) pruneBackups() error {
-	_, ok := os.LookupEnv("BACKUP_RETENTION_DAYS")
-	if !ok {
+	retention := os.Getenv("BACKUP_RETENTION_DAYS")
+	if retention == "" {
 		return nil
 	}
 	return errors.New("pruneBackups: not implemented yet")
@@ -198,4 +208,32 @@ func fileExists(location string) (bool, error) {
 		return false, err
 	}
 	return err == nil, nil
+}
+
+func must(f func() error) func() {
+	return func() {
+		if err := f(); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func copy(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
 }
