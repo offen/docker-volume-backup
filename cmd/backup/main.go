ab8970e29a9cb11da6231f2d8a30b1513776aed4
Kilian Lienekampf <klieneka+github@posteo.de>
2022-01-22 10:46:26 +0100

    fix: password AND username must be defined
    not password OR username

--- cmd/backup/main.go
+++ cmd/backup/main.go
@@ -219,7 +219,7 @@ func newScript() (*script, error) {
 	// WebDAV check for env variables
 	// WebDAV instanciate client
 	if s.c.WebdavUrl != "" {
-		if s.c.WebdavUsername == "" && s.c.WebdavPassword == "" {
+		if s.c.WebdavUsername == "" || s.c.WebdavPassword == "" {
 			return nil, errors.New("newScript: WEBDAV_URL is defined, but no credentials were provided")
 		} else {
 			webdavClient := gowebdav.NewClient(s.c.WebdavUrl, s.c.WebdavUsername, s.c.WebdavPassword)
