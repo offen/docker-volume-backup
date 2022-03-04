1202db2e9e44fcdb0733a07c8ad2fb0228f3dd1c
Frederik Ring <frederik.ring@gmail.com>
2022-03-04 13:38:58 +0100

    Define default case first in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,20 +5,20 @@
 
 set -e
 
-if [ -d "/etc/dockervolumebackup/conf.d" ]; then
+if [ ! -d "/etc/dockervolumebackup/conf.d" ]; then
+  BACKUP_CRON_EXPRESSION="${BACKUP_CRON_EXPRESSION:-@daily}"
+
+  echo "Installing cron.d entry with expression $BACKUP_CRON_EXPRESSION."
+  echo "$BACKUP_CRON_EXPRESSION backup 2>&1" | crontab -
+else
   echo "/etc/dockervolumebackup/conf.d was found, using configuration files from this directory."
 
   for file in /etc/dockervolumebackup/conf.d/*; do
     source $file
     BACKUP_CRON_EXPRESSION="${BACKUP_CRON_EXPRESSION:-@daily}"
-    echo "Installing cron.d entry with expression $BACKUP_CRON_EXPRESSION."
+    echo "Appending cron.d entry with expression $BACKUP_CRON_EXPRESSION and configuration file $file"
     (crontab -l; echo "$BACKUP_CRON_EXPRESSION /bin/sh -c 'set -a; source $file; set +a && backup' 2>&1") | crontab -
   done
-else
-  BACKUP_CRON_EXPRESSION="${BACKUP_CRON_EXPRESSION:-@daily}"
-
-  echo "Installing cron.d entry with expression $BACKUP_CRON_EXPRESSION."
-  echo "$BACKUP_CRON_EXPRESSION backup 2>&1" | crontab -
 fi
 
 echo "Starting cron in foreground."
