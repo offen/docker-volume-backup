0ef98a08de594fadd22fbceb5a7c3f1519e0ef88
Frederik Ring <frederik.ring@gmail.com>
2022-06-23 09:37:14 +0200

    Isolate SSH test case

--- test/ssh/run.sh
+++ test/ssh/run.sh
@@ -4,11 +4,11 @@ set -e
 
 cd "$(dirname "$0")"
 
+ssh-keygen -t rsa -m pem -b 4096 -N "test1234" -f id_rsa -C "docker-volume-backup@local"
+
 docker-compose up -d
 sleep 5
 
-# A symlink for a known file in the volume is created so the test can check
-# whether symlinks are preserved on backup.
 docker-compose exec backup backup
 
 sleep 5
@@ -19,13 +19,12 @@ if [ "$(docker-compose ps -q | wc -l)" != "3" ]; then
 fi
 echo "[TEST:PASS] All containers running post backup."
 
-
 docker run --rm -it \
-  -v compose_minio_backup_data:/minio_data \
+  -v compose_ssh_backup_data:/ssh_data \
   alpine \
-  ash -c 'tar -xvf /minio_data/backup/test-hostnametoken.tar.gz.gpg -C /tmp && test -f /tmp/backup/app_data/offen.db'
+  ash -c 'tar -xvf /ssh_data/test-hostnametoken.tar.gz -C /tmp && test -f /tmp/backup/app_data/offen.db'
 
-echo "[TEST:PASS] Found relevant files in untared remote backups."
+echo "[TEST:PASS] Found relevant files in decrypted and untared remote backups."
 
 # The second part of this test checks if backups get deleted when the retention
 # is set to 0 days (which it should not as it would mean all backups get deleted)
@@ -36,10 +35,11 @@ sleep 5
 docker-compose exec backup backup
 
 docker run --rm -it \
-  -v compose_minio_backup_data:/minio_data \
+  -v compose_ssh_backup_data:/ssh_data \
   alpine \
-  ash -c '[ $(find /minio_data/backup/ -type f | wc -l) = "1" ]'
+  ash -c '[ $(find /ssh_data/ -type f | wc -l) = "1" ]'
 
 echo "[TEST:PASS] Remote backups have not been deleted."
 
 docker-compose down --volumes
+rm -f id_rsa id_rsa.pub
