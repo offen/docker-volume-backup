d09aacf3f0b0e747f47be457bf45299d6310734e
Frederik Ring <frederik.ring@gmail.com>
2022-12-23 10:14:37 +0100

    Implement copy for Azure Blob Storage

--- internal/storage/azure/azure.go
+++ internal/storage/azure/azure.go
@@ -4,13 +4,19 @@
 package azure
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"time"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 	"github.com/offen/docker-volume-backup/internal/storage"
 )
 
 type azureBlobStorage struct {
 	*storage.StorageBackend
+	client        *azblob.Client
+	containerName string
 }
 
 // Config contains values that define the configuration of an Azure Blob Storage.
@@ -18,21 +24,46 @@ type Config struct {
 	AccountName       string
 	ContainerName     string
 	PrimaryAccountKey string
+	Endpoint          string
 }
 
-// NewStorageBackend creates and initializes a new S3/Minio storage backend.
+// NewStorageBackend creates and initializes a new Azure Blob Storage backend.
 func NewStorageBackend(opts Config, logFunc storage.Log) (storage.Backend, error) {
-	storage := azureBlobStorage{}
+	cred, err := azblob.NewSharedKeyCredential(opts.AccountName, opts.PrimaryAccountKey)
+	if err != nil {
+		return nil, fmt.Errorf("NewStorageBackend: error creating shared Azure credential: %w", err)
+	}
+	client, err := azblob.NewClientWithSharedKeyCredential(fmt.Sprintf(opts.Endpoint, opts.AccountName), cred, nil)
+	if err != nil {
+		return nil, fmt.Errorf("NewStorageBackend: error creating Azure client: %w", err)
+	}
+	storage := azureBlobStorage{
+		client:        client,
+		containerName: opts.ContainerName,
+	}
 	return &storage, nil
 }
 
 // Name returns the name of the storage backend
-func (v *azureBlobStorage) Name() string {
+func (b *azureBlobStorage) Name() string {
 	return "Azure"
 }
 
 // Copy copies the given file to the storage backend.
 func (b *azureBlobStorage) Copy(file string) error {
+	fileReader, err := os.Open(file)
+	if err != nil {
+		return fmt.Errorf("(*azureBlobStorage).Copy: error opening file %s: %w", file, err)
+	}
+	_, err = b.client.UploadStream(context.TODO(),
+		b.containerName,
+		file,
+		fileReader,
+		nil,
+	)
+	if err != nil {
+		return fmt.Errorf("(*azureBlobStorage).Copy: error uploading file %s: %w", file, err)
+	}
 	return nil
 }
 
