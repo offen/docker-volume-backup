63961cd8266b878680d53618474d080d8ddc02df
Frederik Ring <frederik.ring@gmail.com>
2022-12-30 16:07:34 +0100

    Pass file location to lifecycle commands (#173)
    * Add test case for extending image and calling through to rsync
    
    * Keep backup file location env var
    
    * Add documentation
    
    * Work against untared content in test

---
+++ test/extend/run.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+cd "$(dirname "$0")"
+. ../util.sh
+current_test=$(basename $(pwd))
+
+mkdir -p local
+
+export TEST_VERSION="${TEST_VERSION:-canary}-with-rsync"
+
+docker build . -t offen/docker-volume-backup:$TEST_VERSION
+
+docker-compose up -d
+sleep 5
+
+docker-compose exec backup backup
+
+sleep 5
+
+expect_running_containers "2"
+
+if [ ! -f "./local/app_data/offen.db" ]; then
+  fail "Could not find expected file in untared archive."
+fi
+
+docker-compose down --volumes
