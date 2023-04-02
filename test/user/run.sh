7f20036b15aa4ad04889751c81dbba97f56541d4
Diulgher Artiom <42372666+GeneralTao2@users.noreply.github.com>
2023-04-02 12:12:10 +0300

    Possibility to use -u (user) option in docker exec (#203)
    * Add user option for docker exec
    
    * Add test for user option
    
    * Return test version for image
    
    * remove gitea config file
    
    * refactor tests
    
    * remove comments & fix image name
    
    * add docs
    
    * cleanup
    
    * Update README.md with suggested correction
    
    Co-authored-by: Frederik Ring <frederik.ring@gmail.com>
    
    * fix backup command & bind folder instead of volume
    
    ---------
    
    Co-authored-by: tao <generaltao.md@gmail.com>
    Co-authored-by: Frederik Ring <frederik.ring@gmail.com>

---
+++ test/user/run.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+cd $(dirname $0)
+. ../util.sh
+current_test=$(basename $(pwd))
+
+docker compose up -d
+
+user_name=testuser
+docker exec user-alpine-1 adduser --disabled-password "$user_name"
+
+docker compose exec backup backup
+
+tar -xvf ./local/test.tar.gz
+if [ ! -f ./backup/data/whoami.txt ]; then
+  fail "Could not find file written by pre command."
+fi
+pass "Found expected file."
+
+tar -xvf ./local/test.tar.gz
+if [ "$(cat ./backup/data/whoami.txt)" != "$user_name" ]; then
+  fail "Could not find expected user name."
+fi
+pass "Found expected user."
+
+docker compose down --volumes
+sudo rm -rf ./local
+
