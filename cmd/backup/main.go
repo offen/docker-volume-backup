57e7f2af9eca3319a3f6f5f1a625972a812923d1
Frederik Ring <frederik.ring@gmail.com>
2024-01-28 19:31:09 +0100

    Reflect changes in naming

--- cmd/backup/main.go
+++ cmd/backup/main.go
@@ -47,12 +47,12 @@ func main() {
 	}()
 
 	s.must(s.withLabeledCommands(lifecyclePhaseArchive, func() error {
-		restartContainers, err := s.stopContainersAndServices()
+		restartContainersAndServices, err := s.stopContainersAndServices()
 		// The mechanism for restarting containers is not using hooks as it
 		// should happen as soon as possible (i.e. before uploading backups or
 		// similar).
 		defer func() {
-			s.must(restartContainers())
+			s.must(restartContainersAndServices())
 		}()
 		if err != nil {
 			return err
