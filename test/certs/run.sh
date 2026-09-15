d71ea9b52685f440cb158b74c32be1750ba2fd30
Frederik Ring <frederik.ring@posteo.de>
2026-09-15 19:02:55 +0200

    Port test for self signed certs

--- test/certs/run.sh
+++ test/certs/run.sh
@@ -13,18 +13,18 @@ openssl req -passin pass:test \
   -subj "/C=DE/ST=BE/O=IntegrationTest, Inc." \
   -x509 -new -key "$CERT_DIR/rootCA.key" -sha256 -days 1 -out "$CERT_DIR/rootCA.crt"
 
-openssl genrsa -out "$CERT_DIR/minio.key" 4096
-openssl req -new -sha256 -key "$CERT_DIR/minio.key" \
-  -subj "/C=DE/ST=BE/O=IntegrationTest, Inc./CN=minio" \
-  -out "$CERT_DIR/minio.csr"
+openssl genrsa -out "$CERT_DIR/storage.key" 4096
+openssl req -new -sha256 -key "$CERT_DIR/storage.key" \
+  -subj "/C=DE/ST=BE/O=IntegrationTest, Inc./CN=storage" \
+  -out "$CERT_DIR/storage.csr"
 
 openssl x509 -req -passin pass:test \
-  -in "$CERT_DIR/minio.csr" \
+  -in "$CERT_DIR/storage.csr" \
   -CA "$CERT_DIR/rootCA.crt" -CAkey "$CERT_DIR/rootCA.key" -CAcreateserial \
   -extfile san.cnf \
-  -out "$CERT_DIR/minio.crt" -days 1 -sha256
+  -out "$CERT_DIR/storage.crt" -days 1 -sha256
 
-openssl x509 -in "$CERT_DIR/minio.crt" -noout -text
+openssl x509 -in "$CERT_DIR/storage.crt" -noout -text
 
 docker compose up -d --quiet-pull
 sleep 5
@@ -36,8 +36,8 @@ sleep 5
 expect_running_containers "3"
 
 docker run --rm \
-  -v minio_backup_data:/minio_data \
+  -v storage_data:/storage_data \
   alpine \
-  ash -c 'tar -xvf /minio_data/backup/test.tar.gz -C /tmp && test -f /tmp/backup/app_data/offen.db'
+  ash -c 'tar -xvf /storage_data/backup/test.tar.gz -C /tmp && test -f /tmp/backup/app_data/offen.db'
 
 pass "Found relevant files in untared remote backups."
